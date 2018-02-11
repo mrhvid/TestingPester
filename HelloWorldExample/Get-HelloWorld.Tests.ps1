76909012d5425bf3070554e90c2dce77100ffe9c
Jonas Sommer Nielsen <git@1d.dk>
2018-02-11 16:27:42 +0100

    Germany tests added

--- HelloWorldExample/Get-HelloWorld.Tests.ps1
+++ HelloWorldExample/Get-HelloWorld.Tests.ps1
@@ -3,7 +3,19 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe "Get-HelloWorld" {
-    It "outputs 'Hello World" {
+    It "outputs 'Hello World'" {
         Get-HelloWorld | Should Be 'Hello World'
+    }
+    It "doesn't output 'WTF'" {
+        Get-HelloWorld | Should not be 'WTF'
+    }
+}
+
+Describe "Get-HelloWorld Germany" {
+    It 'outputs "Hello Germany"' {
+        Get-HelloWorld -Country Germany | Should be 'Hello Germany'
+    }
+    It 'Doesn''t output "Hello World"' {
+        Get-HelloWorld -Country Germany | Should not be 'Hello World'
     }
 }
